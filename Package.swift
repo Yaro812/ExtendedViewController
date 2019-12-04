43fc72ec35d77029ad0ec2df9dea691db1cef136
Iaroslav Pasternak <ipasternak@griddynamics.com>
2019-12-04 18:46:04 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -5,24 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "ExtendedViewController",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ExtendedViewController",
-            targets: ["ExtendedViewController"]),
+        name: "ExtendedViewController",
+        targets: ["ExtendedViewController"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ExtendedViewController",
-            dependencies: []),
+        name: "ExtendedViewController",
+        dependencies: []),
         .testTarget(
-            name: "ExtendedViewControllerTests",
-            dependencies: ["ExtendedViewController"]),
-    ]
+        name: "ExtendedViewControllerTests",
+        dependencies: ["ExtendedViewController"]),
+  ]
 )
